552514022cd8606eb4594a7128179d56bba7b822
yuri-note <yuri-note@LAPTOP-NI438OED>
2022-09-05 21:28:00 +0900

    baekjoon_step2

---
+++ BaekJoon_step_2/src/step_2/Baekjoon1330.java
@@ -0,0 +1,22 @@
+package step_2;
+import java.util.*;
+public class Baekjoon1330 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		if (a > b) {
+			System.out.println(">");
+		}
+		else if (a < b) {
+			System.out.println("<");
+		}
+		else if (a == b) {
+			System.out.println("==");
+		}
+
+	}
+
+}
