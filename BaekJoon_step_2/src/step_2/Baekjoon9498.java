c884ebbe67874f34d47062a0f937241d7d5cc5b4
19_web_hyr <19_web_hyr@302-15>
2022-09-05 21:35:03 +0900

    rere

--- BaekJoon_step_2/src/step_2/Baekjoon9498.java
+++ BaekJoon_step_2/src/step_2/Baekjoon9498.java
@@ -6,16 +6,16 @@ public class Baekjoon9498 {
 		
 		int score = scan.nextInt();
 		
-		if (score > 90) {
+		if (score <= 100 && score >= 90) {
 			System.out.println("A");
 		}
-		else if (score > 80) {
+		else if (score >= 80) {
 			System.out.println("B");
 		}
-		else if (score > 70) {
+		else if (score >= 70) {
 			System.out.println("C");
 		}
-		else if (score > 60) {
+		else if (score >= 60) {
 			System.out.println("D");
 		}
 		else {
